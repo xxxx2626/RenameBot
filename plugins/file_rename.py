8e1b4373c75e8aad5af94af57ff64be7deb6df45
amanvishwak <147085734+amanvishwak@users.noreply.github.com>
2024-10-23 11:16:33 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -23,7 +23,7 @@ async def rename_start(client, message):
         
     file = getattr(message, message.media.value)
     filename = file.file_name  
-    if file.file_size > 2000 * 1024 * 1024:
+    if file.file_size > 4000 * 1024 * 1024 * 1024 * 1024:
         return await message.reply_text("Sᴏʀʀy Bʀᴏ Tʜɪꜱ Bᴏᴛ Iꜱ Dᴏᴇꜱɴ'ᴛ Sᴜᴩᴩᴏʀᴛ Uᴩʟᴏᴀᴅɪɴɢ Fɪʟᴇꜱ Bɪɢɢᴇʀ Tʜᴀɴ 2Gʙ")
 
     try:
