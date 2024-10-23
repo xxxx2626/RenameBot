85ae5a38b616974371026302ce24b233944d27cd
amanvishwak <147085734+amanvishwak@users.noreply.github.com>
2024-10-23 20:00:10 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.enums import MessageMediaType
 from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply
-
+import humanize
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
