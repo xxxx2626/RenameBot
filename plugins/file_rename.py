5e818683e4b368f1671c63fb7502b86ebf3130e6
amanvishwak <147085734+amanvishwak@users.noreply.github.com>
2024-10-27 06:50:40 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -27,7 +27,7 @@ async def rename_start(client, message):
         return await message.reply_photo(
                 photo="https://envs.sh/Som.jpg",
                 caption=f"<b>ᴛʜɪs ʙᴏᴛ ɪs ɴᴏᴛ sᴜᴘᴘᴏʀᴛ 4ɢʙ ᴘʟᴇᴀsᴇ ᴜsᴇ ᴏᴜʀ ᴅᴇᴅɪᴄᴀᴛᴇᴅ 4ɢʙ ʙᴏᴛ ✨</b>", reply_markup=InlineKeyboardMarkup([[
-                InlineKeyboardButton("no", url='t.me/')]])
+                InlineKeyboardButton("no", url='t.me/')]]))
 
     try:
         await message.reply_text(
